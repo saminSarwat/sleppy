d4c0e5f9b9f0e49486fc5a83fbfcf415588a52de
saminSarwat <136252613+saminSarwat@users.noreply.github.com>
2023-06-11 23:42:34 +0800

    Add files via upload

---
+++ Larger and small no detect.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num1,num2;
+cout << "enter 2 numbers" <<endl;
+cin >>num1 >>num2;
+if (num1>num2){
+    cout << "Larger number is "<<num1 << endl;
+}
+else{
+            cout << "Larger number is "<<num2;
+}
+
+}
