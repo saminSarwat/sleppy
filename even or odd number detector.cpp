1a82332fbf516b0d9e9888cdef49b14c7ddc79ec
saminSarwat <136252613+saminSarwat@users.noreply.github.com>
2023-06-11 23:31:19 +0800

    Add files via upload

---
+++ even or odd number detector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num;
+cout << "ENTER A NUMBER" <<endl;
+cin >>num;
+if(num%2==0){
+    cout <<"even";
+}
+else {
+    cout<< "odd";
+}
+}
