bb62ee1743032f6928c1e85bc49af9b51dd581cf
saminSarwat <136252613+saminSarwat@users.noreply.github.com>
2023-06-11 23:59:48 +0800

    Add files via upload

---
+++ pass or fail calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int marks;
+    cout << "put your marks"<<endl;
+    cin >> marks;
+    if(marks>=40)
+    {cout << "pass";}
+    else
+    {
+        cout<< "fail";
+    }
+
+}
+
+
