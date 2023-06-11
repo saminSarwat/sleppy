4dd89fbe9b19b6c99446cc57275cc7bb00b89337
saminSarwat <136252613+saminSarwat@users.noreply.github.com>
2023-06-11 23:23:02 +0800

    Add files via upload
    My first copied code lol

---
+++ positive or negative.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num;
+cout << "Enter a whole number" <<endl;
+cin >> num;
+if(num>0){
+    cout << "postive";
+}
+if(num <0)
+{
+    cout << "negative";
+}
+}
